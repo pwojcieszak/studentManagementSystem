e122d1c7a76c303a0aea88a3857d7efc3f07752e
Piotr Wojcieszak <piotrekw1002@gmail.com>
2023-05-16 15:19:52 +0200

    Minor change in Student constructor

--- src/main/java/com/example/studentmanagementsystem/entity/Student.java
+++ src/main/java/com/example/studentmanagementsystem/entity/Student.java
@@ -18,8 +18,8 @@ public class Student {
     public Student(){
 
     }
-    public Student(Long id, String firstName, String lastName, String email) {
-        this.id = id;
+    public Student(String firstName, String lastName, String email) {
+        super();
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
