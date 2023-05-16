bd1fd22b62e2cc2a6486e692ac211d770086faaf
Piotr Wojcieszak <piotrekw1002@gmail.com>
2023-05-16 19:24:41 +0200

    Added Update option

--- src/main/java/com/example/studentmanagementsystem/controller/StudentController.java
+++ src/main/java/com/example/studentmanagementsystem/controller/StudentController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -22,6 +23,7 @@ public class StudentController {
         model.addAttribute("students", studentService.getAllStudents());
         return "students";
     }
+
     @GetMapping("/students/new")
     public String createStudentForm(Model model) {
         Student student = new Student();
@@ -30,8 +32,26 @@ public class StudentController {
     }
 
     @PostMapping("/students")
-    public String saveStudent(@ModelAttribute("student") Student student){
+    public String saveStudent(@ModelAttribute("student") Student student) {
         studentService.saveStudent(student);
         return "redirect:students";
+    }
+
+    @GetMapping("/students/edit/{id}")
+    public String editStudentForm(@PathVariable Long id, Model model) {
+        model.addAttribute("student", studentService.getStudentById(id));
+        return "edit_student";
+    }
+
+    @PostMapping("/students/{id}")
+    public String updateStudent(@PathVariable Long id, @ModelAttribute("student") Student student, Model model) {
+        Student existingStudent = studentService.getStudentById(id);
+        existingStudent.setId(student.getId());
+        existingStudent.setFirstName(student.getFirstName());
+        existingStudent.setLastName(student.getLastName());
+        existingStudent.setEmail(student.getEmail());
+
+        studentService.updateStudent(existingStudent);
+        return "redirect:/students";
     }
 }
